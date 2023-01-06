6cee4b62461dca4c87f23492c388593cfcec2697
Mat Hayward <mat@madebykind.com>
2023-01-06 16:21:55 +0000

    Revert "2.2"
    This reverts commit dac5efbfecd705283a42f7783a171bc2a2e3a506.

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Current version: $(<VERSION)"
+echo "Enter new version number?"
+read v
+bundle exec jekyll build
+git checkout _site
+mv _site ..
+rm -rf ./*
+mv ../_site/* .
+rm -rf ../_site
+git add .
+git commit -am "$v"
+git push
+git checkout develop
+rm VERSION
+touch VERSION
+echo "$v" >> VERSION
