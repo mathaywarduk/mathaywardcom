769e6ed74749f472ece9f6dcf28bc3eff7dd8630
Mat Hayward <mat@madebykind.com>
2023-01-06 16:09:40 +0000

    make versioning easier

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-echo "New version number?"
+echo "Current version: $(<VERSION)"
+echo "Enter new version number?"
 read v
 bundle exec jekyll build
 git checkout _site
@@ -10,3 +11,6 @@ rm -rf ../_site
 git commit -am "$v"
 git push
 git checkout develop
+rm VERSION
+touch VERSION
+echo "$v" >> VERSION
