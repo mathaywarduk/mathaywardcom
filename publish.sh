1f1779eba287ec4c15d491e8c69127f1ec3fcfe1
Mat Hayward <mat@madebykind.com>
2023-01-11 22:39:16 +0000

    update publish script

--- publish.sh
+++ publish.sh
@@ -4,18 +4,16 @@ echo "Enter new version number?"
 read v
 bundle exec jekyll build
 mv node_modules ..
-
 git checkout _site
 mv _site ..
 rm -rf ./*
 mv ../_site/* .
 rm -rf ../_site
 git add .
-git commit -am "$v"
+git commit -m "$v"
 git push
-
 git checkout develop
 mv ../node_modules .
-
+rm VERSION
 touch VERSION
 echo "$v" >> VERSION
